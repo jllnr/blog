b918a61633b211889c4606da3796855ba6fd1f2e
jllnr <58467465+jllnr@users.noreply.github.com>
2020-01-12 15:27:09 +0800

    Add files via upload

--- posts/urls.py
+++ posts/urls.py
@@ -6,17 +6,30 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name="list"),
     path('create/', views.post_create),
-    #path('detail/(?P<title>\d+)/', views.post_detail),
-    #path('<title:id>/', views.post_detail),
-    #re_path('detail/(?P<id>\d+)/', views.post_detail),
-    re_path('(?P<id>\d+)/', views.post_detail),
-    #re_path('(?P<id>\d+)/edit/', views.post_update, name="update"),
-    #re_path('update/(?P<id>\d+)/', views.post_update, name="update"),
-    #path('<str:slug>/edit/', views.post_update, name="update"),
-    #path('update/', views.post_update, name="update"),
-    #re_path('(?P<id>\d+/update/', views.post_update, name="update"),
-    #re_path('update/(?P<id>\d+)/', views.post_update, name="update"),
-    #path('(?P<id>\d+)/delete/', views.post_delete),
+    path('<int:id>/', views.post_detail),
     path('update/<int:id>/', views.post_update, name="update"),
     path('<int:id>/delete/', views.post_delete)
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+#path('detail/(?P<title>\d+)/', views.post_detail),
+#path('<title:id>/', views.post_detail),
+#re_path('detail/(?P<id>\d+)/', views.post_detail),
+#re_path('(?P<id>\d+)/edit/', views.post_update, name="update"),
+#re_path('update/(?P<id>\d+)/', views.post_update, name="update"),
+#path('<str:slug>/edit/', views.post_update, name="update"),
+#path('update/', views.post_update, name="update"),
+#re_path('(?P<id>\d+/update/', views.post_update, name="update"),
+#re_path('update/(?P<id>\d+)/', views.post_update, name="update"),
+#path('(?P<id>\d+)/delete/', views.post_delete),
--- posts/views.py
+++ posts/views.py
@@ -12,10 +12,6 @@ def post_create(request):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
-        #return HttpResponseRedirect(instance.get_absolute_url())
-    #if request.method == "post":
-    #    print(request.post.get("content"))
-    #    print(request.post.get("title"))
 
     context = {
         "form": form,
@@ -23,12 +19,10 @@ def post_create(request):
     return render(request, "post_form.html", context)
 
 def post_detail(request, id=None):
-    #instance = Post.objects.get(id=3)
     instance = get_object_or_404(Post, id=id)
     context = {
     "title": instance.title,
     "instance": instance,
-    #"pagedetail": pagedetail,
     }
     return render(request, "post_detail.html", context)
 
@@ -40,37 +34,23 @@ def post_list(request):
     }
     return render(request, "index.html", context)
 
-#def post_update(request, id=None):
-def post_update(request):
+def post_update(request, id=None):
     instance = get_object_or_404(Post, id=id)
     form = PostForm(request.POST or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
         return HttpResponseRedirect(instance.get_absolute_url())
-
     context = {
     "title": instance.title,
     "instance": instance,
     "form": form
     }
-    #edit_form = "post_form.html"
-    #return render(request, edit_form, context)
-    return render(request, "post_form.html")
-#    return render(request, "index.html", context)
-
-#def post_update(request, id=None):
-#    obj = get_object_or_404(BlogPost, slug=slug)
-#    form = PostForm(request.POST or None, instance=obj)
-#    if form.is_valid():
-#        form.save()
-#    context = {"title": f"Update {obj.title}", "form": form}
-#    return render(request, "post_form.html", context)
-#    return HttpResponse("<h1>EDIT</h1>")
+    return render(request, "post_form.html", context)
 
 
 def post_delete(request, id=None):
     instance = get_object_or_404(Post, id=id)
     instance.delete()
-    messagges.success(request, "Successfully deleted")
+    #messagges.success(request, "Successfully deleted")
     return redirect("posts:list")
